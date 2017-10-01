8d9f4cdfe65bd52bac658abe02783df4eba01577
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-10-01 14:22:02 +0900

    fix, can't get elapsed time

--- src/framework/ViewContainer.ts
+++ src/framework/ViewContainer.ts
@@ -48,7 +48,7 @@ abstract class ViewContainer extends Container implements State {
     }
 
     protected get elapsedTimeMillis(): number {
-        return this.elapsedTimeMillis;
+        return this._elapsedTimeMillis;
     }
 
     public get backGroundLayer(): Container {
