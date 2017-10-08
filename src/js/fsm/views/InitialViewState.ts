ada3ebc81ac0006a4cad94d9b863f422d6b291aa
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-10-08 16:51:48 +0900

    show load progress animation

--- src/js/fsm/views/InitialViewState.ts
+++ src/js/fsm/views/InitialViewState.ts
@@ -1,13 +1,18 @@
-import {dispatchEvent} from '../../../framework/EventUtils';
+import {addEvents, dispatchEvent, removeEvents} from '../../../framework/EventUtils';
 import ViewContainer from "../../../framework/ViewContainer";
 import Deliverable from "../../../framework/Deliverable";
-import AssetLoader, {Asset} from "../../../framework/AssetLoader";
+import AssetLoader from "../../../framework/AssetLoader";
 
 import {Events as ApplicationEvents} from "../ApplicationState";
 
+import LoadingAnimationContainer from "../../texture/containers/LoadingAnimationContainer";
+
 import imageManifest from '../../resources/image';
+import {SKIP_BRAND_LOGO_ANIMATION} from "../../Constants";
 
 export enum Events {
+    COMPLETE_PRELOAD = "InitialViewState@COMPLETE_LOAD",
+    COMPLETE_LOGO_ANIMATION = "InitialViewState@COMPLETE_LOGO_ANIMATION",
 }
 
 class InitialViewState extends ViewContainer {
@@ -15,12 +20,10 @@ class InitialViewState extends ViewContainer {
 
     private _loader: AssetLoader;
 
-    /**
-     * @override
-     */
-    update(elapsedTime: number): void {
+    private _loadingAnimation: LoadingAnimationContainer;
 
-    }
+    private _isLoadComplete: boolean;
+    private _isLogoAnimationComplete: boolean;
 
     /**
      * @override
@@ -28,15 +31,33 @@ class InitialViewState extends ViewContainer {
     onEnter(params: Deliverable): void {
         super.onEnter(params);
 
+        addEvents({
+            [Events.COMPLETE_PRELOAD]: this._handleLoadCompleteEvent,
+            [Events.COMPLETE_LOGO_ANIMATION]: this._handleLogoAnimCompleteEvent,
+        });
+
         // TODO: check logged-in.
         // TODO: check device is iOS.
-        // TODO: load resources.
+
+        this._isLoadComplete = false;
+        this._isLogoAnimationComplete = SKIP_BRAND_LOGO_ANIMATION ? true : false;
+
+        this._loadingAnimation = new LoadingAnimationContainer(this.viewWidth, this.viewHeight);
+        this._loadingAnimation.position.set(this.viewWidth * 0.5, this.viewHeight * 0.5);
+
+        this.applicationLayer.addChild(
+            this._loadingAnimation,
+        );
 
         this._loader = new AssetLoader();
         this._loader.setImageManifest(imageManifest);
         // this._loader.setSoundManifest(soundManifest);
-        // this._loader.onProgress.add(this._onLoadProgress);
-        this._loader.load(this._onLoadComplete);
+        this._loader.onProgress.add(this._onLoadProgress);
+        this._loader.load(() => dispatchEvent(Events.COMPLETE_PRELOAD));
+
+        this._loadingAnimation
+            .start()
+            .then(() => !SKIP_BRAND_LOGO_ANIMATION && dispatchEvent(Events.COMPLETE_LOGO_ANIMATION));
     }
 
     /**
@@ -44,16 +65,45 @@ class InitialViewState extends ViewContainer {
      */
     onExit(): void {
         super.onExit();
+
+        removeEvents([
+            Events.COMPLETE_PRELOAD,
+            Events.COMPLETE_LOGO_ANIMATION,
+        ]);
     }
+
+    /**
+     *
+     * @param {AssetLoader} event
+     * @private
+     */
+    private _onLoadProgress = (event: AssetLoader): void => {
+        const percentage = event.progress;
+        this._loadingAnimation.progress(percentage);
+    };
 
     /**
      *
      * @private
      */
-    private _onLoadComplete = (loader: AssetLoader, resources: { string: Asset }): void => {
-        console.log(`Complete to load [${Object.keys(resources).length}] resources.`);
+    private _handleLoadCompleteEvent = () => {
+        this._isLoadComplete = true;
 
-        dispatchEvent(ApplicationEvents.INITIALIZED);
+        if (this._isLoadComplete && this._isLogoAnimationComplete) {
+            dispatchEvent(ApplicationEvents.INITIALIZED);
+        }
+    };
+
+    /**
+     *
+     * @private
+     */
+    private _handleLogoAnimCompleteEvent = () => {
+        this._isLogoAnimationComplete = true;
+
+        if (this._isLoadComplete && this._isLogoAnimationComplete) {
+            dispatchEvent(ApplicationEvents.INITIALIZED);
+        }
     };
 }
 
