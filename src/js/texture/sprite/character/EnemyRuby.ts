692fde9edfb9778798b5aa619b0e50ac73866308
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-11-28 12:32:46 +0900

    edit ruby texture index

--- src/js/texture/sprite/character/EnemyRuby.ts
+++ src/js/texture/sprite/character/EnemyRuby.ts
@@ -12,10 +12,10 @@ const FRAMES: FrameStructureIndexes = {
     WAIT: [0, 1, 2],
     TRY_ATTACK: [4],
     SUCCESS_ATTACK: [3],
-    LOSE: [8, 9, 10, 11, 12, 11, 12, 11, 10, 9],
+    LOSE: [8, 9],
     WIN: [5, 6, 7],
-    LOSE_TEXTURE: 4,
-    WIN_TEXTURE: 6,
+    LOSE_TEXTURE: 8,
+    WIN_TEXTURE: 5,
 };
 
 /**
