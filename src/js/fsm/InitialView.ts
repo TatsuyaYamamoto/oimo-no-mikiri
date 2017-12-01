629b39b9deb2a81d777465ece4b73655af6b65ce
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-12-01 22:09:56 +0900

    implement iOS init tap event

--- src/js/fsm/InitialView.ts
+++ src/js/fsm/InitialView.ts
@@ -2,16 +2,21 @@ import {addEvents, dispatchEvent, removeEvents} from '../../framework/EventUtils
 import ViewContainer from "../../framework/ViewContainer";
 import Deliverable from "../../framework/Deliverable";
 import AssetLoader from "../../framework/AssetLoader";
+import {t} from "../../framework/i18n";
 
 import {Events as ApplicationEvents} from "./ApplicationState";
 
 import LoadingAnimationContainer from "../texture/containers/LoadingAnimationContainer";
+import Text from "../texture/internal/Text";
 
 import imageManifest from '../resources/image';
 import soundManifest from '../resources/sound';
+import {Ids as StringIds} from '../resources/string';
+
 import {SKIP_BRAND_LOGO_ANIMATION} from "../Constants";
 
 import {Category, TimingVariable, trackPageView, trackTiming, VirtualPageViews} from "../helper/tracker";
+import {isIOS} from "../../framework/utils";
 
 export enum Events {
     COMPLETE_PRELOAD = "InitialViewState@COMPLETE_LOAD",
@@ -22,6 +27,7 @@ class InitialViewState extends ViewContainer {
     private _loader: AssetLoader;
 
     private _loadingAnimation: LoadingAnimationContainer;
+    private _tapInfoText: Text;
 
     private _isLoadComplete: boolean;
     private _isLogoAnimationComplete: boolean;
@@ -41,8 +47,70 @@ class InitialViewState extends ViewContainer {
         });
 
         // TODO: check logged-in.
-        // TODO: check device is iOS.
 
+        if (isIOS()) {
+            this._showIosInitInfo();
+        } else {
+            this._startPreload();
+        }
+    }
+
+    /**
+     * @override
+     */
+    onExit(): void {
+        super.onExit();
+
+        removeEvents([
+            Events.COMPLETE_PRELOAD,
+            Events.COMPLETE_LOGO_ANIMATION,
+        ]);
+    }
+
+    private _showIosInitInfo = () =>{
+        this._tapInfoText = new Text(t(StringIds.TAP_DISPLAY_INFO), {
+            fontSize: 40,
+            stroke: '#ffffff',
+            strokeThickness: 2,
+        });
+        this._tapInfoText.position.set(this.viewWidth * 0.5, this.viewHeight * 0.5);
+        this.addClickWindowEventListener(this._onIosInitClicked);
+
+        this.applicationLayer.addChild(
+            this._tapInfoText,
+        );
+    };
+
+    private _onIosInitClicked = () => {
+        this.removeClickWindowEventListener(this._onIosInitClicked);
+        this.applicationLayer.removeChildren();
+
+        this._startPreload();
+    };
+
+    /**
+     *
+     * @param {AssetLoader} event
+     * @private
+     */
+    private _onLoadProgress = (event: AssetLoader): void => {
+        const percentage = event.progress;
+        this._loadingAnimation.progress(percentage);
+    };
+
+    /**
+     *
+     * @private
+     */
+    private _handleLoadCompleteEvent = () => {
+        this._isLoadComplete = true;
+
+        if (this._isLoadComplete && this._isLogoAnimationComplete) {
+            dispatchEvent(ApplicationEvents.INITIALIZED);
+        }
+    };
+
+    private _startPreload = () => {
         this._isLoadComplete = false;
         this._isLogoAnimationComplete = SKIP_BRAND_LOGO_ANIMATION ? true : false;
 
@@ -66,40 +134,6 @@ class InitialViewState extends ViewContainer {
         this._loadingAnimation
             .start()
             .then(() => !SKIP_BRAND_LOGO_ANIMATION && dispatchEvent(Events.COMPLETE_LOGO_ANIMATION));
-    }
-
-    /**
-     * @override
-     */
-    onExit(): void {
-        super.onExit();
-
-        removeEvents([
-            Events.COMPLETE_PRELOAD,
-            Events.COMPLETE_LOGO_ANIMATION,
-        ]);
-    }
-
-    /**
-     *
-     * @param {AssetLoader} event
-     * @private
-     */
-    private _onLoadProgress = (event: AssetLoader): void => {
-        const percentage = event.progress;
-        this._loadingAnimation.progress(percentage);
-    };
-
-    /**
-     *
-     * @private
-     */
-    private _handleLoadCompleteEvent = () => {
-        this._isLoadComplete = true;
-
-        if (this._isLoadComplete && this._isLogoAnimationComplete) {
-            dispatchEvent(ApplicationEvents.INITIALIZED);
-        }
     };
 
     /**
