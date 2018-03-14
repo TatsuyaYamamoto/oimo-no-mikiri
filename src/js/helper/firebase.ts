48dee3c62498eb3febfe48cb15c803459942520d
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-03-14 23:29:08 +0900

    remove useless method

--- src/js/helper/firebase.ts
+++ src/js/helper/firebase.ts
@@ -24,37 +24,3 @@ export function init() {
 
     auth().signInAnonymously();
 }
-
-export async function requestCreateGame() {
-    return post(`http://localhost:5000/oimo-no-mikiri-development/us-central1/app/createGame`)
-        .then(res => res.json())
-        .then(json => json.gameId);
-}
-
-export async function requestJoinGame(gameId: string) {
-    return post(`http://localhost:5000/oimo-no-mikiri-development/us-central1/app/joinGame`, {
-        gameId
-    });
-}
-
-async function post(input: string, json?: any) {
-    const fetchInit = {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${await getToken()}`
-        },
-        body: JSON.stringify(json)
-    };
-    if (json) {
-        fetchInit.headers["Content-Type"] = "application/json";
-    }
-
-    return fetch(input, fetchInit);
-}
-
-async function getToken(forceRefresh?: boolean) {
-    const token = await auth().currentUser.getIdToken(forceRefresh);
-    console.log(`Got Firebase Token! ${token.slice(0, 10)}...`);
-
-    return token;
-}
